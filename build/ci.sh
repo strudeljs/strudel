96df9c6aa1a385fd0b7cdb0860dfdc5086ca19bf
Mateusz Luczak <mateusz.luczak@cognifide.com>
2018-06-29 14:18:54 +0200

    chore: CI build fix

--- build/ci.sh
+++ build/ci.sh
@@ -1,5 +1,5 @@
 set -e
-if [[ -z $CI_PULL_REQUEST ]] && [[ $CIRCLE_BRANCH = master ]]; then
+if [[ -z $CI_PULL_REQUEST ]] || [[ $CIRCLE_BRANCH = master ]]; then
   npm run build
   npm run test:unit
   cd sc-4.4.9-linux && ./bin/sc -u $SAUCE_USER -k $SAUCE_ACCESS_KEY -f ~/sauce_is_ready &
