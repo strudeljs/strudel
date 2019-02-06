de2ef4843b4077f75a835f98a56e97e3982f1468
Miłosław Politowski <miloslaw.politowski@gmail.com>
2019-02-06 14:27:46 +0100

    test: add test for removing event listener for specific callback
    #29

--- test/unit/features/dom/element.spec.js
+++ test/unit/features/dom/element.spec.js
@@ -1,3 +1,4 @@
+import Component from '../../../../src/decorators/component';
 import Element from '../../../../src/dom/element';
 
 describe('Element', () => {
@@ -51,5 +52,25 @@ describe('Element', () => {
       expect(Element('div').get(1).innerHTML).toEqual('Second element')
       expect(Element('div').get().length).toEqual(2);
     })
+  });
+
+  describe('events', () => {
+    it('removes specific event callback', () => {
+      document.body.innerHTML = `
+        <button>Button</button>
+      `;
+
+      const method = () => 'method';
+      const spy = jasmine.createSpy();
+      const element = new Element('button');
+
+      element.on('click', spy);
+      element.on('click', method);
+      element.off('click', spy);
+      element.trigger('click')
+
+      expect(element.get(0)._e.click[0][0]).toEqual('method');
+      expect(spy).not.toHaveBeenCalled();
+    });
   });
 });
