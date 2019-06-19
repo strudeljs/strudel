ee44a7b0c0d2fba67dff5a11f92bc148ef3fb3e7
Mateusz Luczak <mateusz.luczak@cognifide.com>
2019-06-19 16:10:36 +0200

    fix(onInit): Resolved issue when OnInit used with init caused call stack exceeding

--- src/decorators/onInit.js
+++ src/decorators/onInit.js
@@ -4,10 +4,13 @@ import { createDecorator } from '../util/helpers';
  * OnInit decorator - sets method to be run at init
  * @returns {Function} decorator
  */
-
 export default createDecorator((component, property) => {
   const emptyFnc = function () {};
   const org = component.init || emptyFnc;
+
+  if (property === 'init') {
+    return;
+  }
 
   component.init = function (...args) {
     component[property].apply(this, ...args);
